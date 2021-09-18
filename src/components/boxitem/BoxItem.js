aa8909fd3e267d6fc7f804ac8da9cb154b757e48
hikarocarvalho <hikarofcarvalho@gmail.com>
2021-09-18 17:27:38 -0300

    update

--- src/components/boxitem/BoxItem.js
+++ src/components/boxitem/BoxItem.js
@@ -1,11 +1,66 @@
-import React from "react";
+import React,{useState,useEffect} from "react";
 import "./BoxItem.css";
+import Modal from "./../../components/modal/Modal";
+import { ProjectApi } from "../../api/api_projects";
+import DateTransform from "./../../tools/DateTransform";
+import { Link } from "react-router-dom";
 
-const BoxItem = ({children}) => {
+const BoxItem = (props) => {
+    const [open,setOpen] = useState(false);
+    const project = props.project;
+    const userid = props.userid;
+    const cancel = ()=>{
+        console.log(cancel);
+        setOpen(false);
+    };
+    const remove = ()=>{
+        setOpen(true);
+    };
+    const exclude = async()=>{
+        const response = await ProjectApi.fetchDelete(project._id);
+        const data = await response;
+        console.log(data[0])
+        setOpen(false);
+        window.location.reload();
+    };
    
     return(
-            <article >
-                {children}
+            <article className="boxitem-link">
+                <Link to={`/tasks/${project._id}/${userid}`} className="box">
+                        
+                        <header className="titleproject">
+                           
+                            {project.project_title}
+                            {project.project_priority==="High"?
+                            (<div className="red"></div>):project.project_priority==="Midle"?
+                            (<div className="yellow"></div>):project.project_priority==="Low"?
+                            (<div className="green"></div>):null}
+                            
+                        </header>
+                        <div className="description">
+                            <hr/>
+                                {project.project_description}
+                            <hr/>
+                                this project has be created at:
+                                <br/>
+                                  
+                                {DateTransform(project.project_date)}
+                            <hr/>
+                                this project have to be done at:
+                                <br/>
+                                  
+                                {DateTransform(project.project_final_date)}
+                        </div>
+                        
+                    </Link>
+                     <button className="trash"  onClick={remove} ></button>
+                     {open?
+                        <Modal>
+                            <header>Do you really want to exclude this?</header>
+                            <a href="#" onClick={exclude} className="confirm">Yes</a>
+                            <a href="#" onClick={cancel} className="cancel">No</a>
+                        </Modal>
+                    :null}
             </article>
        
     );
--- src/pages/projects/index.js
+++ src/pages/projects/index.js
@@ -6,39 +6,33 @@ import BoxItem from "../../components/boxitem/BoxItem";
 import { ProjectApi } from "../../api/api_projects";
 import { useParams } from "react-router";
 import { Link } from "react-router-dom";
-import DateTransform from "./../../tools/DateTransform";
-import { useHistory } from "react-router-dom";
-import Modal from "./../../components/modal/Modal";
+
 function Projects(){
-    const history = useHistory();
     const [projects,setprojects] = useState([]);
-    const [open,setOpen] = useState(false);
+   
     
     useEffect(()=>{
         getproject();
     },[])
-     const remove = ()=>{
-        setOpen(true);
-    };
-    const exclude = async(id)=>{
-        const response = await ProjectApi.fetchDelete(id);
-        const data = await response;
-        console.log(data[0])
-        setOpen(false);
-        window.location.reload();
-    };
-    const cancel = ()=>{
-        console.log(cancel);
-        setOpen(false);
-    };
+    
+   
     const userid= useParams().userid;
 
     const getproject = async ()=>{
-
-        const response = await ProjectApi.fetchByUser(userid);
-        console.log(response);
-        const data = await response.json();
-        setprojects(data);
+        try{
+            const response = await ProjectApi.fetchByUser(userid);
+            if(response.status === 200){
+                console.log(response);
+                const data = await response.json();
+                setprojects(data);
+                console.log(data);
+            }else{
+                console.log("erro");
+            }
+            
+        }catch(error){
+            console.log("error");
+        }
     }
     
     return(
@@ -46,53 +40,20 @@ function Projects(){
             <MenuBar id="menutitle" title="Projects"/>
             <BoxComponents>
                 {projects.map((project,index)=>(
-                    <BoxItem key={project._id} className="boxitem" >
-                    <Link to={`/tasks/${project._id}/${userid}`} className="boxitem-link">
-                        
-                        <header className="titleproject">
-                           
-                            {project.project_title}
-                            {project.project_priority==="High"?
-                            (<div className="red"></div>):project.project_priority==="Midle"?
-                            (<div className="yellow"></div>):project.project_priority==="Low"?
-                            (<div className="green"></div>):null}
-                            
-                        </header>
-                        <div className="description">
-                            <hr/>
-                                {project.project_description}
-                            <hr/>
-                                this project has be created at:
-                                <br/>
-                                  
-                                {DateTransform(project.project_date)}
-                            <hr/>
-                                this project have to be done at:
-                                <br/>
-                                  
-                                {DateTransform(project.project_final_date)}
-                        </div>
-                        
-                    </Link>
-                     <button className="trash" onClick={remove()}  ></button>
-                     {open?
-                        <Modal>
-                            <header>Do you really want to exclude this?</header>
-                            <a href="#" onClick={exclude(project._id)} className="confirm">Yes</a>
-                            <a href="#" onClick={cancel} className="cancel">No</a>
-                        </Modal>
-                    :null}
+                    <BoxItem key={project._id} project={project} className="boxitem" userid={userid}>
+                    
+
                      </BoxItem>
                 ))}
                 <Link to={"/projects/registerproject/"+userid} className="boxitem-link">
-                <BoxItem className="boxitem" >
+                
                     <div className="description">
                         <label className="add">+</label>
                     </div>
-                </BoxItem>
+              
                 </Link>
                 
-            
+               
             </BoxComponents>
         </div>
         
